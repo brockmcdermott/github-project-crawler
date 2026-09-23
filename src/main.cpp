dcd03afce4d859b4ae7c467ce1bd908f26ac405b
jameson irwin <170916278+jakair1201@users.noreply.github.com>
2026-09-23 15:11:53 -0600

    Enhance main.cpp with JSON parsing and tests
    Added JSON parsing and tests for SearchOptions and Repository.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include "search_options.h"
+#include "repository.h"
+#include <nlohmann/json.hpp>
+
+
 
 int main()
 {
-    std::cout << "GitHub Project Crawler is ready to crawl." << '\n';
+    //testcmake edit                 **raw string literal below, easier than the alternative**
+    nlohmann::json test_json = nlohmann::json::parse(R"({"name": "hello"})");
+    std::cout << "parsed name: " << test_json["name"] << "\n";
+
+    //simple test for search_options.h
+    SearchOptions options;
+    std::cout << "defualt query: " << options.query << ", count: " << options.count << "\n";
+
+    //simple test for search_options.h
+    Repository test;
+    test.owner = "linus";
+    test.name = "struct test";
+    test.stars = 1234567;
+    std::cout << test.owner << "/" << test.name << " has " << test.stars << " stars\n";
+
     return 0;
 }
